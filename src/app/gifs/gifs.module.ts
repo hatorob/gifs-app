8ca5e54e0d8d3a6568ea02ead7ce6c3efcbbafa8
Hector Toro <alejotoro94@hotmail.com>
2024-03-04 10:29:28 -0500

    Se agrega componente lazy-image

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
-import { HomePageComponent } from './pages/home/home-page.component';
-import { ShearchBoxComponent } from './components/shearch-box/shearch-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { CardComponent } from './components/card/card.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { ShearchBoxComponent } from './components/shearch-box/shearch-box.component';
 
 
 
 @NgModule({
   declarations: [
+    CardListComponent,
+    CardComponent,
     HomePageComponent,
     ShearchBoxComponent,
-    CardListComponent,
-    CardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
   ]
 })
+
 export class GifsModule { }
