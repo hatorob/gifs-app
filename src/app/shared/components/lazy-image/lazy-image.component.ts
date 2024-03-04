4fd837800015df59523f254e95c5fa66bdab12c2
Hector Toro <alejotoro94@hotmail.com>
2024-03-04 11:25:22 -0500

    Se sube el loader y se le da animaciones a las imagenes del gif

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public title!: string;
+
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('the url is required.');
+  }
+
+  onload() {
+    setTimeout( () => {
+      this.hasLoaded = true;
+    },1000)
+  }
+
 
 }
